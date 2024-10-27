f2416131e793bcb830a86b5d7ee5660a9eedc25e
Christmann-Julian <julianchristmann@laposte.net>
2024-10-27 14:33:05 +0100

    Mise en place du router

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/style.css';
+import ErrorPage from "./pages/ErrorPage";
+import Homepage from "./pages/Homepage";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 function App() {
   return (
     <>
-      
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ front/src/pages/ErrorPage.jsx
@@ -0,0 +1,36 @@
+import { useRouteError } from "react-router-dom";
+import '../assets/css/errors.css';
+
+const ErrorPage = () => {
+  const error = useRouteError();
+
+  return (
+    <section className="error-page">
+      <div className="table">
+        <div className="table-cell">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6 offset-lg-3 col-12">
+                <div className="error-inner">
+                  <h2>404</h2>
+                  <h5>Cette page est introuvable</h5>
+                  <p>
+                    Oups ! La page que vous recherchez n'existe pas. Elle a
+                    peut-être été déplacée ou supprimée.
+                  </p>
+                  <div className="button">
+                    <a href="#" className="btn">
+                      Retourner à l'accueil
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ErrorPage;
