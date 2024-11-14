4819cdff275c996b8c3dc65debcd19e2762c2192
bilal <bilal-rockstar@outlook.com>
2024-11-14 16:41:45 +0100

    Défilement dynamique du carroussel d'imahe dans la page d'accueil

--- front/src/components/HeroSlider.jsx
+++ front/src/components/HeroSlider.jsx
@@ -1,5 +1,5 @@
 // src/components/HeroSlider.js
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../assets/css/hero-slider.css';
 import Bureau from '../assets/img/bureau.jpg';
 import Clock from '../assets/img/clock.jpg';
@@ -13,34 +13,48 @@ function HeroSlider() {
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
+  const intervalRef = useRef(null);
+
+  const startCarousel = () => {
+    intervalRef.current = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+    }, 5000); // Change every 5 seconds
+  };
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === images.length - 1 ? 0 : prevIndex + 1
-      );
-    }, 5000);
-    return () => clearInterval(interval);
-  }, [images.length]);
+    startCarousel(); // Start the carousel on mount
+    return () => clearInterval(intervalRef.current); // Clear interval on unmount
+  }, []);
+
+  const resetCarouselInterval = () => {
+    clearInterval(intervalRef.current); // Clear the existing interval
+    startCarousel(); // Restart the interval
+  };
 
   const goToPrevious = () => {
     setCurrentIndex(currentIndex === 0 ? images.length - 1 : currentIndex - 1);
+    resetCarouselInterval();
   };
 
   const goToNext = () => {
     setCurrentIndex(currentIndex === images.length - 1 ? 0 : currentIndex + 1);
+    resetCarouselInterval();
   };
 
   return (
     <section className="hero-slider">
-      <div className="single-slider">
-        <img src={images[currentIndex].src} alt="Slide" />
-        <div className="text-inner">
-          <h1>{images[currentIndex].caption}</h1>
-        </div>
-        <button className="carousel-control prev" onClick={goToPrevious}>&#10094;</button>
-        <button className="carousel-control next" onClick={goToNext}>&#10095;</button>
+      <div className="slider-container" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+        {images.map((image, index) => (
+          <div className="slide" key={index}>
+            <img src={image.src} alt="Slide" />
+            <div className="text-inner">
+              <h1><span>{image.caption}</span></h1>
+            </div>
+          </div>
+        ))}
       </div>
+      <button className="carousel-control prev" onClick={goToPrevious}>&#10094;</button>
+      <button className="carousel-control next" onClick={goToNext}>&#10095;</button>
     </section>
   );
 }
