be2ef9ee995d85ce9946d001ee76e91b596b4cce
mbenazza <m.benazza@lprs.fr>
2025-02-10 16:32:53 +0100

    fix logo mobile

--- front/src/pages/admin/CustomLayout.jsx
+++ front/src/pages/admin/CustomLayout.jsx
@@ -1,29 +1,32 @@
 import { Layout } from 'react-admin';
 import { AppBar } from 'react-admin';
 import { Typography } from '@mui/material';
-import { Box } from '@mui/material';
-import Logo from '../../assets/img/cyna-logo-white.png'; 
+import { Box, useMediaQuery } from '@mui/material';
+import Logo from '../../assets/img/cyna-logo-white.png';
 
-const CustomAppBar = (props) => (
-    <AppBar {...props} sx={{ backgroundColor: "#302082" }}>
-        
-        <Typography variant="h6" color="inherit">
-            Cyna Admin
-        </Typography>
-        <Box 
-            sx={{ 
-                flex: 1, 
-                display: 'flex', 
-                justifyContent: 'center', 
-                alignItems: 'center',
-                height: '100%'
-            }}
-        >
-            <img src={Logo} alt="Logo" style={{ height: 40 }} />
-        </Box>
-        
-    </AppBar>
-);
+const CustomAppBar = (props) => {
+    const isMobile = useMediaQuery('(max-width:600px)'); 
+    return (
+        <AppBar {...props} sx={{ backgroundColor: "#302082" }}>
+            <Typography variant="h6" color="inherit">
+                Cyna Admin
+            </Typography>
+            <Box
+                sx={{
+                    flex: 1,
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '100%'
+                }}
+            >
+                 {!isMobile && (
+                    <img src={Logo} alt="Logo" style={{ height: 40 }} />
+                )}
+            </Box>            
+        </AppBar>
+    )
+}
 
 const CustomLayout = (props) => <Layout {...props} appBar={CustomAppBar} />;
 
